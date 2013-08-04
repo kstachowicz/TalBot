bb50fd1510570594eb88a8cd8d9f2fe6a272e488
Kamil Stachowicz <k.stachowicz@hotmail.com>
2013-08-04 22:39:32 +0200

    knowledge base with hardcoded questions and answers

--- app/main.py
+++ app/main.py
@@ -1,24 +1,29 @@
 import random
 
-sample_responses = []
+sample_responses = ["mhm", "and then what..?", "that's interesting", "tell me more :)"]
 
-def prepare_sample_responses():
-	global sample_responses
-	sample_responses = ["mhm", "and then what..?", "that's interesting", "tell me more :)"]
-	"""print sample_responses[:]"""
+knowledge_base = {"who are you?": ["I am Tala. And I'm a bot.", "And you?", "Not you business."],
+				"www": ["I'm from Poland", "From home", "From internet ;)"],
+				"how old are you?": ["100", "200", "300"]}
 
 def get_sample_response():
 	count = len(sample_responses)
 	return sample_responses[random.randint(0,count-1)]
 
+def get_answer(question):
+	if question in knowledge_base:
+		count = len(knowledge_base[question])
+		return knowledge_base[question][random.randint(0,count-1)]
+	else:
+		return get_sample_response()
+
 def main():
-	prepare_sample_responses()
-	answer = ""
+	question = ""
 	print "Hi, How can I help you?"
-	while answer != "0":
-		answer = raw_input()
-		response = get_sample_response()
-		print response
+	while question != "0":
+		question = raw_input()
+		answer = get_answer(question)
+		print answer
 
 
 if __name__ == "__main__":
