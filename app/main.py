f6cc184a84b36ce2463972f5ea913702f4a70c6e
Kamil Stachowicz <k.stachowicz@hotmail.com>
2013-08-05 11:46:46 +0200

    clearify question, save previous question

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ import random
 
 sample_responses = ["mhm", "and then what..?", "that's interesting", "tell me more :)"]
 
-knowledge_base = {"who are you?": ["I am Tala. And I'm a bot.", "And you?", "Not you business."],
-				"www": ["I'm from Poland", "From home", "From internet ;)"],
-				"how old are you?": ["100", "200", "300"]}
+knowledge_base = {"WHO ARE YOU": ["I am Tala. And I'm a bot.", "And you?", "Not you business."],
+				"WWW": ["I'm from Poland", "From home", "From internet ;)"],
+				"HOW OLD ARE YOU": ["100", "200", "300"]}
+
+punctuation = '.?!'
 
 def get_sample_response():
 	count = len(sample_responses)
@@ -17,11 +19,31 @@ def get_answer(question):
 	else:
 		return get_sample_response()
 
+def clear_question(question):
+	for sym in punctuation:
+		question = question.replace(sym,'')
+
+	return question
+
+
+def process_question(question):
+	question = question.upper()
+	clearified_question = clear_question(question)
+	
+	return clearified_question
+
 def main():
 	question = ""
+	previous_question = ""
 	print "Hi, How can I help you?"
 	while question != "0":
 		question = raw_input()
+		question = process_question(question)
+		if previous_question == question:
+			print "I've just told you!"
+			continue;
+
+		previous_question = question	
 		answer = get_answer(question)
 		print answer
 
